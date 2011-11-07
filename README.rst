ce97df815b279bbc98a8eee73a3e8f6b7d12298b
Jonas Baumann <jone@jone.ch>
2011-11-07 03:15:26 +0100

    Created package.

---
+++ README.rst
@@ -0,0 +1,10 @@
+Introduction
+============
+
+
+
+The content listing tile lists content in a deco page, based on predefined queries.
+It is based on `plone.app.contentlisting`_.
+
+
+.. _plone.app.contentlisting: http://pypi.python.org/pypi/plone.app.contentlisting
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='plone.app.contentlistingtile',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
+      keywords='',
+      author='Plone Foundation',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
